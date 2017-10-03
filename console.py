fa9da5207b549b7c95be585a606aeb2d0a2f719f
Anoop M <noop2016@gmail.com>
2017-10-03 04:28:31 +0000

    added completion feature to create

--- console.py
+++ console.py
@@ -4,24 +4,93 @@ Entry point of the command interpreter
 AirBnB Version 1
 """
 import cmd
+from models import storage
 from models.base_model import BaseModel
 
-class HBNBCommand(cmd.Cmd):
-    """Command Line Class"""
 
+
+class HBNBCommand(cmd.Cmd):
+    """ Command Line Class """
+
+    intro = "=====Holberton AirBnB Console 0.0.1====="
     prompt = "(hbhb) "
+    __instances = storage.all()
 
     def do_quit(self, line):
-        """quit command"""
+        """ Exits the HBNB Console """
         quit()
 
     def do_EOF(self, line):
-        """exit"""
+        """ If a command handler returns a true value, the program will exit cleanly. """
         return (True)
 
     def emptyline(self):
-        """enter empty line"""
+        """ Ignores empty line """
         pass
+
+    # Main Functions
+
+    def do_create(self, cls_name):
+        """
+        Creates a new instance of `BaseModel`
+
+        Usage: create [class name]
+
+        """
+        if not cls_name:
+            print("** class name missing **")
+        else:
+            try:  # test if class exist
+                instance = eval(cls_name)()
+            except NameError:
+                print("** class doesn't exist **")
+            else:
+                print(instance.id)
+        return
+
+    def complete_create(self, text, line, begidx, endidx):
+
+        instances = list(self.__instances.keys())
+        cls_names = list(set((name.split('.')[0] for name in instances)))
+        if not text:
+            completions = cls_names[:]
+        else:
+            completions = [ f
+                            for f in cls_names
+                            if f.startswith(text)
+                            ]
+        return completions
+
+    def do_show(self, args):
+        """
+        Prints the string representation of an instance
+
+        Usage: show [class name] [id]
+        """
+
+        if not args:
+            print("** class name missing **")
+            return
+
+        try:  # test for second argument
+            cls_name, cls_id = args.split(' ')
+        except ValueError:
+            print("** instance id missing **")
+            return
+        else:
+            if cls_name != "BaseModel":
+                print("** class doesn't exist **")
+                return
+
+        inst = cls_name + '.' + cls_id
+        if inst in self.__instances:
+            print(self.__instances[inst])
+        else:
+            print("** no instance found **")
+        return
+
+
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
--- models/base_model.py
+++ models/base_model.py
@@ -40,7 +40,7 @@ class BaseModel:
     def save(self):
         """ updates public instance attribute `updated_at` """
         self.updated_at = datetime.now()
-        storage.save()
+        storage.save()  # save all the instances in scope
 
     def to_dict(self):
         """ Returns: a dict with all keys/values of __dict__ of instance """
